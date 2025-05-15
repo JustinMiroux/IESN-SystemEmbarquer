7ba85d143c441107cb31ba74eec729fcbc6874e3
JustinMiroux <justin@miroux.eu>
2025-05-15 11:28:38 +0200

    Update lora c

--- Steve/lora_controller/lora_controller.ino
+++ Steve/lora_controller/lora_controller.ino
@@ -16,12 +16,12 @@ void setup()
   digitalWrite(RFM95_RST, HIGH);
 
   Serial.begin(115200);
+
   while (!Serial) {
     delay(1);
   }
 
   delay(100);
-
   Serial.println("Feather LoRa TX Test!");
 
   // manual reset
@@ -35,46 +35,45 @@ void setup()
     Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
+
   Serial.println("LoRa radio init OK!");
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     while (1);
   }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 5 to 23 dBm:
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
   rf95.setTxPower(23, false);
+
 }
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
+int16_t packetnum = 0;
 
-int distance; //variable contenant distance reçue par robot
-// String Buffer = "";
+int distance;
+String Buffer = "";
+int mode = 0;
+
 void loop()
 {
-  // if (Serial.available() > 0) {
+  if (Serial.available() > 0) {
 
-  //   Buffer = Serial.readStringUntil('\r\n');
-  //   Serial.println(Buffer);
+    Buffer = Serial.readStringUntil('\r\n');
     
-  // }
+  }
   int xAxis = analogRead(A0); // Read Joysticks X-axis
   int yAxis = analogRead(A1); // Read Joysticks Y-axis
-
-  delay(10); // Wait 1 second between transmits, could also 'sleep' here!
-  //Serial.println("Transmitting..."); // Send a message to rf95_server
   
-  //char radiopacket[] = "Bonjour c'est justin";
-  int radiopacket[2] = {xAxis, yAxis};
-  //itoa(packetnum++, radiopacket+13, 10);
-  //Serial.print("Sending "); Serial.println(radiopacket[0]);
-  //radiopacket[] = 0;
+  if (Buffer == "auto") {
+    mode = 1;
+  }
+
+  else if (Buffer == "manual") {
+    mode = 0;
+  }
+
+  delay(10);
+  int radiopacket[3] = {xAxis, yAxis};
   
   Serial.println("Sending..."); Serial.println(xAxis); Serial.println(yAxis);
   delay(10);
@@ -82,26 +81,23 @@ void loop()
   delay(10);
   rf95.waitPacketSent();
 
-
-  // Now wait for a reply
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
 
-  //Serial.println("Waiting for reply...");
   if (rf95.waitAvailableTimeout(1000))
   { 
-    // Should be a reply message for us now   
+
     if (rf95.recv(buf, &len))
-   {
-    distance = buf[0] + buf[1]*256;
-    //Serial.println("Distance: ");
-    Serial.println(distance);
-    //Serial.println("cm");    
+    {
+    
+      distance = buf[0] + buf[1]*256;
+      Serial.println(distance);
+    
     }
+
     else
     {
       Serial.println("Receive failed");
     }
   } 
 }
-//wesh
